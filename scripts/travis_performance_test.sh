78c1d8127d3d9bd4ab68d3872273d6ee9a1d9e67
zaf6862 <zaidahmed6862@gmail.com>
2019-07-23 00:33:53 -0400

    added performance test for msocket in travis

--- scripts/travis_performance_test.sh
+++ scripts/travis_performance_test.sh
@@ -5,6 +5,8 @@ javac -cp ../jars/msocket-1.0.0.jar:. runmsocketserverclient.java
 java -cp ../jars/msocket-1.0.0.jar:. runmsocketserverclient > mstime.txt
 javac runtcpserverclient.java;
 java runtcpserverclient > tcptime.txt
+head mstime.txt
+head tcptime.txt
 difference=`python3 check_performance_diff.py`
 echo $difference
 rm mstime.txt tcptime.txt *.class
